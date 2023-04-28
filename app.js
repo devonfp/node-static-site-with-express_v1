d6ab7f705e021a9ea1a74c2737f688f404b0981c
Devon Poston <devonposton1@gmail.com>
2023-04-28 15:30:28 -0400

    Setup code and added nodemon

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const data =  require('./data.json');
+
+const app = express();
+const path = require('path');
+
+
+// view engine setup
+app.set('view engine', 'pug');
+app.use('/static', express.static(path.join(__dirname, 'public')));
+// Explain how to code above works
+
+/* GET home and about page. */
+router.get('/', function(req, res, next) {
+    // 1. Pass all project data to 'index' template
+    res.render('index',{projects: data.projects});
+  });
+
+  router.get('/about', function(req, res, next) {
+    // 1. Pass all project data to 'index' template
+    res.render('about', {projects: data.projects});
+  });
+
+
+  router.get('/projects/:id', function(req, res, next) {
+    const projectId = req.params.id;
+    const project = projects.find( ({ id }) => id === +projectId );
+    
+    if (project) {
+      res.render('project', { project });
+    } else {
+      const error = new Error('Project not found');
+      error.status = 404;
+      next(error);
+    }
+  });
+
+  
+
+// Set up middleware to catch undefined routes
+app.use((req, res, next) => {
+  const error = new Error('Sorry, the page you requested could not be found.');
+  error.status = 404;
+  next(error);
+});
+
+// Set up error handling middleware
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message });
+});
+
+
+// Start server
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+
+
+ module.exports = router;
